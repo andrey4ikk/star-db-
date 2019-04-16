562e8e8fbae7568f4923a617fe51d4a00ae40db3
andrey4ikk <andd1.duka@gmail.com>
2019-04-16 20:02:43 +0300

    transform api

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -24,11 +24,12 @@ export default class SwapiService {
   
     async getAllPlanets() {
       const res = await this.getResource(`/planets/`);
-      return res.results;
+      return res.results.map(this._transformPlanet);
     }
   
-    getPlanet(id) {
-      return this.getResource(`/planets/${id}/`);
+    async getPlanet(id) {
+      const planet = await this.getResource(`/planets/${id}/`);
+      return this._transformPlanet(planet);
     }
   
     async getAllStarships() {
@@ -38,5 +39,13 @@ export default class SwapiService {
   
     getStarship(id) {
       return this.getResource(`/starships/${id}/`);
+    }
+    _transformPlanet(planet) {
+      return {
+          name: planet.name,
+          population: planet.population,
+          rotationPeriod: planet.rotation_period,
+          diameter: planet.diameter
+      }
     }
   }
